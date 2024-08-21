68f1d82e941efe408e92513bc0bc8f70f11431c2
KaitoTLex <warrenlin1688@gmail.com>
2024-08-20 17:11:31 -0700

    Added: Latex.lua, ui.lua

---
+++ lua/plugins/latex.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "lervag/vimtex",
+    ft = { "tex", "latex", "bib" },
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here
+      if vim.fn.executable("zathura") == 1 then
+        vim.g.vimtex_view_method = "zathura"
+      else
+        vim.g.vimtex_view_method = "skim"
+      end
+      vim.g.vimtex_compiler_method = "latexmk"
+    end,
+  },
+}
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+        hover = {
+          enabled = true,
+          silent = true, -- set to true to not show a message if hover is not available
+          view = nil,    -- when nil, use defaults from documentation
+          ---@type NoiceViewOptions
+          opts = {},     -- merged with defaults from documentation
+        },
+      },
+    },
+  },
+  {
+    "folke/zen-mode.nvim",
+    config = function()
+      require("zen-mode").setup {}
+      vim.api.nvim_set_keymap('n', '<Space>wz', ':ZenMode<CR>', { noremap = true, silent = true })
+    end
+  }
+}
